01978375fdeca4126dd4b634c894e3782d871225
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-07-30 19:26:30 -0300

    header done

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CheckSquare } from 'react-feather';
+import { PlusCircle } from 'react-feather';
+import './header.css';
+
+export default function Header(props) {
+
+
+    return (
+        <header className="container header">
+            <div className="logo">
+                <p>To-Do List</p>
+                <CheckSquare color="#00008a" size={25} />
+            </div>
+            <div className="add-icon">
+                <PlusCircle color="#00008a" size={30} />
+            </div>
+        </header>
+    )
+}
